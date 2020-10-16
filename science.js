1588c5618aef1ba1e82150ecad3a6ff3cc277110
yeseo-del <72997064+yeseo-del@users.noreply.github.com>
2020-10-16 15:04:03 -0500

    Update science.js

--- science.js
+++ science.js
@@ -51,25 +51,25 @@ function getLabT5(){
 }
 
 function updateLabCost(){
-	labWoodCost = Math.floor(10 * Math.pow(1.1,lab) * labT1Multi);
-	labGemCost = Math.floor(15 * Math.pow(1.1,lab) * labT1Multi);
-	labMetalCost = Math.floor(20 * Math.pow(1.1,lab) * labT1Multi);
+	labWoodCost = Math.floor(1 * Math.pow(1.1,lab) * labT1Multi);
+	labGemCost = Math.floor(1 * Math.pow(1.1,lab) * labT1Multi);
+	labMetalCost = Math.floor(1 * Math.pow(1.1,lab) * labT1Multi);
 
-	labT2WoodCost = Math.floor(500 * Math.pow(1.1,labT2) * labT2PlusMulti);
-	labT2GemCost = Math.floor(200 * Math.pow(1.1,labT2) * labT2PlusMulti);
-	labT2MetalCost = Math.floor(1000 * Math.pow(1.1,labT2) * labT2PlusMulti);
+	labT2WoodCost = Math.floor(1 * Math.pow(1.1,labT2) * labT2PlusMulti);
+	labT2GemCost = Math.floor(1 * Math.pow(1.1,labT2) * labT2PlusMulti);
+	labT2MetalCost = Math.floor(1 * Math.pow(1.1,labT2) * labT2PlusMulti);
 
-	labT3WoodCost = Math.floor(9600 * Math.pow(1.1,labT3) * labT2PlusMulti);
-	labT3GemCost = Math.floor(4700 * Math.pow(1.1,labT3) * labT2PlusMulti);
-	labT3MetalCost = Math.floor(17000 * Math.pow(1.1,labT3) * labT2PlusMulti);
+	labT3WoodCost = Math.floor(1 * Math.pow(1.1,labT3) * labT2PlusMulti);
+	labT3GemCost = Math.floor(1 * Math.pow(1.1,labT3) * labT2PlusMulti);
+	labT3MetalCost = Math.floor(1 * Math.pow(1.1,labT3) * labT2PlusMulti);
 
-	labT4MetalCost = Math.floor(610000 * Math.pow(1.1,labT4) * labT2PlusMulti);
-	labT4GemCost = Math.floor(37000 * Math.pow(1.1,labT4) * labT2PlusMulti);
-	labT4WoodCost = Math.floor(926000 * Math.pow(1.1,labT4) * labT2PlusMulti);
+	labT4MetalCost = Math.floor(1 * Math.pow(1.1,labT4) * labT2PlusMulti);
+	labT4GemCost = Math.floor(1 * Math.pow(1.1,labT4) * labT2PlusMulti);
+	labT4WoodCost = Math.floor(1 * Math.pow(1.1,labT4) * labT2PlusMulti);
 
-	labT5MetalCost = Math.floor(12400000 * Math.pow(1.1,labT5) * labT2PlusMulti);
-	labT5GemCost = Math.floor(7300000 * Math.pow(1.1,labT5) * labT2PlusMulti);
-	labT5WoodCost = Math.floor(15900000 * Math.pow(1.1,labT5) * labT2PlusMulti);
+	labT5MetalCost = Math.floor(1 * Math.pow(1.1,labT5) * labT2PlusMulti);
+	labT5GemCost = Math.floor(1 * Math.pow(1.1,labT5) * labT2PlusMulti);
+	labT5WoodCost = Math.floor(1 * Math.pow(1.1,labT5) * labT2PlusMulti);
 }
 
 function purchaseTech(id) {
